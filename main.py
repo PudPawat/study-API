8d9e607f74bf6d61326c6958784565a655424423
kittichanr <petchkubbb@gmail.com>
2024-09-16 14:20:04 +0700

    add cors allow all origin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import json
 import random
@@ -7,6 +8,14 @@ import string
 from typing import List
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def health_check():
